d7be8875d137e561b9eed17bb56d3b3008d5bd1e
Svenja Jetzinger <svenjaj@uia.no>
2025-08-18 15:19:46 +0200

    lab1 video exercise

--- assignment_#. Ex. nnoori/IKT213_noori/assignment_1/assignment1SvenjaJetzinger.py
+++ assignment_#. Ex. nnoori/IKT213_noori/assignment_1/assignment1SvenjaJetzinger.py
@@ -5,6 +5,8 @@ def main():
     path = 'assignment_#. Ex. nnoori/IKT213_noori/assignment_1'
     img = cv2.imread(str(path) + '/lena-1.png')             # read an image
     print_image_information(img)
+
+    video()
     
 
 def print_image_information(img):
@@ -14,6 +16,22 @@ def print_image_information(img):
     print('channels: ' + str(img.shape[2]))                 # print the number of channels in the image
     print('size: ' + str(img.size))                         # print the total number of values in the array
     print('data type: ' + str(img.dtype))                   # print the data type of the image array
+
+def video():
+    
+    cam = cv2.VideoCapture(0)                               # Open the default camera
+    
+    # frame per seconds
+    fps = cam.get(cv2.CAP_PROP_FPS)                         # Get the frames per second of the camera       
+    print('FPS: ' + str(fps))                               # Print the frames per second (fps)
+
+    # frame width and height
+    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    print('Frame Width: ' + str(frame_width))               # Print the width of the frame
+    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print('Frame Height: ' + str(frame_height))             # Print the height of the frame
+
+    cam.release()                                           # Release (close) the camera
 
 main()
 

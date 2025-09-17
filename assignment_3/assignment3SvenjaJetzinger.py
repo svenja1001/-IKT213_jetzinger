ba5d7528d12e580a416df978b501359f6f2a7d43
Svenja Jetzinger <svenjaj@uia.no>
2025-09-17 11:19:29 +0200

    main function usw.

--- assignment_3/assignment3SvenjaJetzinger.py
+++ assignment_3/assignment3SvenjaJetzinger.py
@@ -1,1 +1,15 @@
-# start
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+def main():
+    path = 'assignment_3'
+    img = cv2.imread(str(path) + '/lambo.png')                     # read/load the image
+    
+    
+
+
+
+
+
+main()
